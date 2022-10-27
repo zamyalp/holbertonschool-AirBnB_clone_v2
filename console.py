6d63281732ceb08e0745504c0de6cc5043f1cb33
zamyalp <zamyalp03@gmail.com>
2022-10-26 18:27:29 -0700

    console update

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 """ Console Module """
 import cmd
 import sys
-from shlex import split 
+from shlex import split
+from symbol import parameters
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -118,35 +119,24 @@ class HBNBCommand(cmd.Cmd):
         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
         Create a new class instance with given keys/values and print its id.
         """
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
-
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                kwargs[key] = value
-
-            if kwargs == {}:
-                obj = eval(my_list[0])()
-            else:
-                obj = eval(my_list[0])(**kwargs)
-                storage.new(obj)
-            print(obj.id)
-            obj.save()
-
-        except SyntaxError:
+        parameters = args.split(' ')
+        if not args:
             print("** class name missing **")
-        except NameError:
+            return
+        if parameters[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[parameters[0]]()
+        for item in parameters[1:]:
+            attribute = item.split("=")[1].replace('"', '').replace("_", " ")
+            try:
+                attribute = eval(attribute)
+            except Exception:
+                pass
+            setattr(new_instance, item.split("=")[0], attribute)
+        new_instance.save()
+        print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
