26fcd96322840ad39c4903c7ff1864ed24985635
zamyalp <zamyalp03@gmail.com>
2022-10-28 08:54:49 -0700

    dumb

--- models/place.py
+++ models/place.py
@@ -101,3 +101,5 @@ class Place(BaseModel, Base):
                 if value.place_id == self.id:
                     reviews_of_place.append(value)
             return reviews_of_place
+places = relationship("Place", backref='user', cascade='delete')
+    reviews = relationship("Review", backref='user', cascade='delete')
