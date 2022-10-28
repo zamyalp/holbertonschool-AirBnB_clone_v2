11648574bfb9e43dd542d4a8ce465f3ccb9904f3
Weirdo23 <98723905+zamyalp@users.noreply.github.com>
2022-10-28 09:42:31 -0500

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-import os
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
