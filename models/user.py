bf7aab8a253477016909836ae977b7ace765af0f
Weirdo23 <98723905+zamyalp@users.noreply.github.com>
2022-10-28 14:59:18 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,7 +9,10 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
