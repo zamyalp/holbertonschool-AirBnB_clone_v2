3ad7e36896040901ebd1062cf2c57f815da35164
zamyalp <zamyalp03@gmail.com>
2022-11-15 15:06:57 -0800

    work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Starts a Flask web application"""
 
 from flask import Flask
@@ -29,4 +28,4 @@ def hello_hbnb():
 
 if __name__ == "__main__":
 >>>>>>> 7b13fa883303f4b45ec1f248ea9606bc13e0b01a
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
