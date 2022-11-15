4444d5eb5ef223722fc8186b8cbb544e2c782df6
zamyalp <zamyalp03@gmail.com>
2022-11-15 15:10:07 -0800

    work please

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,22 +10,5 @@ def hello_hbnb():
     """Returns a string at the root route"""
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-=======
-"""this will start a flask web application tuned
-to 0.0.0.0, port 5000"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """this displays "hello hbnb"""
-    return "Hello HBNB!"
-
-
 if __name__ == "__main__":
->>>>>>> 7b13fa883303f4b45ec1f248ea9606bc13e0b01a
     app.run(host="0.0.0.0", port=5000)
