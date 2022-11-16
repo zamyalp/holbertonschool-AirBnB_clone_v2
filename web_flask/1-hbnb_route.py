a2f5f18459437f7b1564aed37c993105e8e392e5
zamyalp <zamyalp03@gmail.com>
2022-11-15 16:00:51 -0800

    work or ill be sad

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,11 +10,12 @@ def hello_hbnb():
     """Returns a string at the root route"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
     """Returns a string at the root route"""
     return 'HBNB'
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
