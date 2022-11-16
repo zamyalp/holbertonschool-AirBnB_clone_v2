92aa5029b6f779f1d44967a0ca9a53dede3e3ab2
zamyalp <zamyalp03@gmail.com>
2022-11-15 17:23:07 -0800

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns a string at the root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """Returns a string at the root route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_2(text):
+    """ Function that displays C followed by the value of the text"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb_3(text):
+    """ Function that displays python """
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
