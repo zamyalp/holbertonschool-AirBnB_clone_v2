db0a6bc3462d528114d38919f17c2b3f6cdf9de1
zamyalp <zamyalp03@gmail.com>
2022-11-16 17:44:33 -0800

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def hbnb_4(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def show_page(n):
-    """this will display an html page"""
+    """ this will display an html page """
     return render_template('5-number.html', n=n)
 
 
