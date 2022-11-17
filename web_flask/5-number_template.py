933ca7d0eaae1f5c6368d68b39c75305c812eb23
zamyalp <zamyalp03@gmail.com>
2022-11-16 17:37:59 -0800

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,10 +38,11 @@ def hbnb_4(n):
     return ("{} is a number".format(n))
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """this will display an html page"""
-    return render_template("5-number.html", n=n)
+    if type(n) is int:
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
