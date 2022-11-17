ed908ad36d02ea0824ad71e0afb68b046fed0488
zamyalp <zamyalp03@gmail.com>
2022-11-16 17:29:58 -0800

    5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns a string at the root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """Returns a string at the root route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_2(text):
+    """ Function that displays C followed by the value of the text"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb_3(text):
+    """ Function that displays python """
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hbnb_4(n):
+    """ Function that shows n is a number """
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """this will display an html page"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
